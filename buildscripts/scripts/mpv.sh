d321c2b1b4d98a3365d37e5021eaaebe1a12f5b5
FoxNick <354435383@qq.com>
2026-05-15 19:34:49 +0800

    Update mpv.sh

--- buildscripts/scripts/mpv.sh
+++ buildscripts/scripts/mpv.sh
@@ -13,22 +13,11 @@ else
 	exit 255
 fi
 
-# Android provides Vulkan, but no pkgconfig file
-# you can double-check the version in vulkan_core.h (-> VK_HEADER_VERSION)
-mkdir -p "$prefix_dir"/lib/pkgconfig
-cat >"$prefix_dir"/lib/pkgconfig/vulkan.pc <<"END"
-Name: Vulkan
-Description:
-Version: 1.3.275
-Libs: -lvulkan
-Cflags:
-END
-
 unset CC CXX # meson wants these unset
 
 meson setup $build --cross-file "$prefix_dir"/crossfile.txt \
 	--default-library shared \
-	-Diconv=disabled -Dlua=enabled -Dvulkan=enabled \
+	-Diconv=disabled -Dlua=enabled \
 	-Dlibmpv=true -Dcplayer=false \
 	-Dmanpage-build=disabled
 
